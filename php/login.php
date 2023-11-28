5b89a997b1afb7e05fb396b0dfdf9136ce571067
Luis Yerga <lyergamayor1@gmail.com>
2023-11-28 14:05:39 +0100

    header y formularios de inicio

--- php/login.php
+++ php/login.php
@@ -4,18 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log In</title>
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+  </script>
 </head>
-<header>
-    <img src="../img/Esteban&Co.png">
-    <h1>Esteban&Co</h1>
-</header>
+
 <body>
-    <div id="InicioSesion">
-        <input type="text" placeholder="Email" id="usuario" name="usuario">
-        <input type="text" placeholder="Contraseña" id="contraseña" name="contraseña">
-        <button type="button">Iniciar Sesión</button>
-        <label>¿Aun no tienes una cuenta con nosotros?</label>
-        <a href="signin.php"><label>Crear nueva cuenta</label></a>
-    </div>
+     <header>
+        <div class="headerAcceso">
+            <img src="../img/Esteban&Co.png">
+            <h1>Esteban&Co</h1>
+        </div>
+    </header>
+    <main>
+        <div id="InicioSesion" class="formInicio">
+            <input type="text" placeholder="Email" id="usuario" name="usuario"> <br>
+            <input type="text" placeholder="Contraseña" id="contraseña" name="contraseña"><br>
+            <button type="button">Iniciar Sesión</button><br>
+            <label>¿Aun no tienes una cuenta con nosotros?</label>
+            <a href="signin.php"><label>Crear nueva cuenta</label></a>
+        </div>
+    </main>
 </body>
 </html>
+
--- php/signin.php
+++ php/signin.php
@@ -4,23 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In</title>
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+  </script>
 </head>
-<header>
-    <img src="../img/Esteban&Co.png">
-    <h1>Esteban&Co</h1>
-</header>
+
 <body>
-    <div id="CrearCuenta">
-        <input type="text" placeholder="Nombre" id="nombre" name="nombre">
-        <input type="text" placeholder="Apellido" id="apellido" name="apellido">
-        <input type="text" placeholder="DNI" id="DNI" name="DNI">
-        <input type="text" placeholder="Email" id="email" name="email">
-        <input type="text" placeholder="Contraseña" id="contraseña" name="contraseña">
-        <input type="text" placeholder="País" id="pais" name="pais">
-        <button type="button">Crear cuenta</button>
-        <label>¿Ya tienes una cuenta?</label>
-        <a href="login.php"><label>Iniciar sesión</label></a>
-        
-    </div>
+    <header>
+        <div class="headerAcceso">
+            <img src="../img/Esteban&Co.png">
+            <h1>Esteban&Co</h1>
+        </div>
+    </header>
+    <main>
+        <div id="CrearCuenta" class="formInicio">
+            <input type="text" placeholder="Nombre" id="nombre" name="nombre"> <br>
+            <input type="text" placeholder="Apellido" id="apellido" name="apellido"><br>
+            <input type="text" placeholder="DNI" id="DNI" name="DNI"><br>
+            <input type="text" placeholder="Email" id="email" name="email"><br>
+            <input type="text" placeholder="Contraseña" id="contraseña" name="contraseña"><br>
+            <input type="text" placeholder="País" id="pais" name="pais"><br>
+            <button type="button">Crear cuenta</button><br>
+            <label>¿Ya tienes una cuenta?</label>
+            <a href="login.php"><label>Iniciar sesión</label></a>
+        </div>
+    </main>
 </body>
 </html>
