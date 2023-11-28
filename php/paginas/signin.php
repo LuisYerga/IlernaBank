6d10ac93a0131931b8e37501da8d748db8610db3
LuisYerga <lyergamayor1@gmail.com>
2023-11-28 20:44:24 +0100

    organización carpetas

--- php/paginas/signin.php
+++ php/paginas/signin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In</title>
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="../../css/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
@@ -19,7 +19,7 @@
 <body>
     <header>
         <div class="headerAcceso">
-            <img src="../img/Esteban&Co.png">
+            <img src="../../img/Esteban&Co.png">
             <h1>Esteban&Co</h1>
         </div>
     </header>
