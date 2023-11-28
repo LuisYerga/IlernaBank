e0630eb130bb9a637f59758655c4e2a8f43d3c40
Luis Yerga <lyergamayor1@gmail.com>
2023-11-28 09:31:06 +0100

    Html Log y Sign in

---
+++ php/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+</head>
+<header>
+    <img src="../img/Esteban&Co.png">
+    <h1>Esteban&Co</h1>
+</header>
+<body>
+    <div id="InicioSesion">
+        <input type="text" placeholder="Email" id="usuario" name="usuario">
+        <input type="text" placeholder="Contraseña" id="contraseña" name="contraseña">
+        <button type="button">Iniciar Sesión</button>
+        <label>¿Aun no tienes una cuenta con nosotros?</label>
+        <a href="signin.php"><label>Crear nueva cuenta</label></a>
+    </div>
+</body>
+</html>
---
+++ php/signin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+</head>
+<header>
+    <img src="../img/Esteban&Co.png">
+    <h1>Esteban&Co</h1>
+</header>
+<body>
+    <div id="CrearCuenta">
+        <input type="text" placeholder="Nombre" id="nombre" name="nombre">
+        <input type="text" placeholder="Apellido" id="apellido" name="apellido">
+        <input type="text" placeholder="DNI" id="DNI" name="DNI">
+        <input type="text" placeholder="Email" id="email" name="email">
+        <input type="text" placeholder="Contraseña" id="contraseña" name="contraseña">
+        <input type="text" placeholder="País" id="pais" name="pais">
+        <button type="button">Crear cuenta</button>
+        <label>¿Ya tienes una cuenta?</label>
+        <a href="login.php"><label>Iniciar sesión</label></a>
+        
+    </div>
+</body>
+</html>
