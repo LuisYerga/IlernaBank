5b2021a7bd2fcac479e91d710bba56774e840122
Luis Yerga <lyergamayor1@gmail.com>
2023-12-01 11:59:02 +0100

    Creación pagina inicio y añadido de imagenes

---
+++ php/paginas/inicioUser.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <link href="../../css/style.css" rel="stylesheet">
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+</head>
+
+<body>
+     <header>
+        <div class="headerPaginas">
+            <img src="../../img/Esteban&Co.png">
+            <h1>Esteban&Co</h1>
+            <img src="../../img/correo.png">
+            <img src="../../img/usu.png">
+        </div>
+    </header>
+    <main>
+        <section class="principal">
+            <div class="container">
+                <h3>Bienvenido de vuelta</h3>
+                <img src="../../img/credito.png">
+                <h5>Tu saldo</h5>
+                <h3>1000000</h3>
+                <h5>Últimos movimientos</h5>
+                <h5>Realizar operación</h5>
+                <button name="Retirar">Retirar dinero</button>
+                <button name="Ingresar">Ingresar dinero</button>
+            </div>
+        </section>
+        <section class="aside">
+        </section>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+  </script>
+</body>
+</html>
