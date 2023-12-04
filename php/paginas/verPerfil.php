ef974ee781c382113711ae7a1c9264838f01bb8d
Luis Yerga <lyergamayor1@gmail.com>
2023-12-04 14:25:24 +0100

    creacion nueva página y correcion header

--- php/paginas/verPerfil.php
+++ php/paginas/verPerfil.php
@@ -3,15 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log In</title>
+    <title>Perfil</title>
     <link href="../../css/style.css" rel="stylesheet">
     <link href="../../css/styleInicio.css" rel="stylesheet">
-  <!-- Bootstrap CSS v5.2.1 -->
+    <!-- Bootstrap CSS v5.2.1 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script defer src="../../js/menu.js"></script>
 </head>
-
 <body>
   <header>
     <div class="headerPaginas">
@@ -22,6 +21,7 @@
         <h1>Esteban&Co</h1>
       </div>
       <div class="iconosMenu">
+        <a class="button" href="inicioUser.php" id="volver"><img src="../../img/salir.png"></a>
         <a class="button" href="" id="correo"><img src="../../img/correo.png"></a>
         <a class="button" id="menu"><img src="../../img/usu.png"></a>
       </div>
@@ -30,7 +30,7 @@
   <aside class="aside" id="aside">
     <div class="head">
       <div id="Perfil" class="opciones">
-        <a class="button option" id="perfil" href="verPerfil.php"><img src="../../img/usuario.png"><p>Mi perfil</p></a>
+        <a class="button option" id="perfil" href=""><img src="../../img/usuario.png"><p>Mi perfil</p></a>
       </div>
       <div id="Buzon" class="opciones">
         <a class="button option" id="buzon" href=""><img src="../../img/correo2.png"><p>Buzón</p></a>
@@ -49,27 +49,5 @@
       </div>
     </div>
   </aside>
-  <main>
-      <section class="principal">
-          <div class="container">
-              <h3>Bienvenido de vuelta</h3>
-              <img src="../../img/credito.png">
-              <h5>Tu saldo</h5>
-              <h3>1000000</h3>
-              <h5>Últimos movimientos</h5>
-              <h5>Realizar operación</h5>
-              <button name="Retirar">Retirar dinero</button>
-              <button name="Ingresar">Ingresar dinero</button>
-          </div>
-      </section>
-  </main>
-
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
-  </script>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
-  </script>
 </body>
 </html>
